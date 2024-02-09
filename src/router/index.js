087e162e72c4fc40beb032acd2306ee048511ce1
oceane <oceane.copub@gmail.com>
2024-02-09 15:09:21 +0100

    class clients

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CategorieView from '../views/CategorieView.vue'
+import ClientsView from "@/views/ClientsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/clients',
+      name: 'clients',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: ClientsView
     }
   ]
 })
---
+++ src/views/ClientsView.vue
@@ -0,0 +1,84 @@
+<template>
+  <main>
+    <div>
+      <h1>Les clients</h1>
+    </div>
+    <div>
+      <table>
+        <caption>Les clients - page 1/10</caption> //ADD NUMBER CHANGES
+        <tr>
+          <th>Code</th>
+          <th>Societe</th>
+          <th>Contact</th>
+          <th>Ville</th>
+        </tr>
+        <!-- Si le tableau des catégories est vide -->
+        <tr v-if="data.listeClients.length === 0">
+          <td colspan="4">Veuillez patienter, chargement des clients...</td>
+        </tr>
+        <!-- Si le tableau des catégories n'est pas vide -->
+        <tr v-for="clients in data.listeClients" :key="clients.code">
+          <td>{{ clients.code }}</td>
+          <td>{{ clients.societe }}</td>
+          <td>{{ clients.contact }}</td>
+          <td>{{ clients.adresse.ville }}</td>
+        </tr>
+        <tr>
+          <td><button>first page</button></td>
+          <td><button>previous</button></td>
+          <td><button>next</button></td>
+          <td><button>last page</button></td>
+        </tr>
+      </table>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { reactive, onMounted } from "vue";
+import { doAjaxRequest } from "@/api";
+
+let data = reactive({
+  // La liste des catégories affichée sous forme de table
+  listeClients: []
+});
+
+function showError(error) {
+  console.log("Erreur : status %d", error.status)
+  console.log(error.body);
+  alert(error.message);
+}
+
+function chargeClients() {
+  // Appel à l'API pour avoir la liste des catégories
+  // Trié par code, descendant
+  // Verbe HTTP GET par défaut
+  doAjaxRequest("/api/clients")
+      .then((json) => {
+        data.listeClients = json._embedded.clients;
+      })
+      .catch(showError);
+}
+
+
+// A l'affichage du composant, on affiche la liste
+onMounted(chargeClients);
+
+</script>
+
+
+<style scoped>
+td,
+th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #232623;
+  color: rgb(255, 255, 255);
+}
+</style>
